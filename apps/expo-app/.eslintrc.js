09d2782902fe5fa3073fe9a69e89ca46f90d4da8
kcrypto126 <technt22@gmail.com>
2025-03-31 23:29:24 -0400

    .

--- apps/expo-app/.eslintrc.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('@kit/eslint-config');
--- apps/expo-app/app/(app)/(main)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-
-export default function HomePage() {
-  return (
-    <View className="flex flex-1 items-center justify-center">
-      {/* Your code goes here */}
-    </View>
-  );
-}
